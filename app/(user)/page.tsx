4ee8ae3d99b1ac88ff088752c6a4d9c2f51f316f
Jordan <jordanporter94@outlook.com>
2023-03-13 10:16:04 +0000

    add previewSuspense export, test groq queries

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,27 @@
+import { previewData } from "next/headers"
+import { groq } from "next-sanity"
+import { client } from "../../lib/sanity.client"
 
-export default function HomePage() {
+const query = groq`
+*[_type=="post"] {
+  ...,
+  author->,
+  categories[]->
+   } | order(_createdAt desc)
+`
+
+export default async function HomePage() {
+
+  if(previewData()) {
+    return <div>Preview Mode</div>
+  }
+
+  const posts = await client.fetch(query)
+  console.log('posts')
+  console.log(posts)
   return (
     <div>
-
+      <h1>Not in Preview Mode</h1>
     </div>
   )
 }
