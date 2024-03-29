16979256f33a69248d5256b20fddac1d4ea43bc1
Jordan <jordanporter94@outlook.com>
2024-03-29 15:59:58 +0000

    updating script

--- modifyPackage.sh
+++ modifyPackage.sh
@@ -3,6 +3,9 @@
 # Define the file path
 file="./node_modules/@sanity/types/lib/dts/src/index.d.ts"
 
+
+echo "$file"
+
 # Define the sed command to remove the 'const' keyword from TType and TName variables
 sed -i '' -e 's/const TType/   TType/' "$file"
 sed -i '' -e 's/const TName/   TName/' "$file"
